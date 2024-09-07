31a9c906221ebb341c1e90b1c18c294e16b29b74
Ilya <40602243+DreXASK@users.noreply.github.com>
2024-09-07 21:16:35 +0300

    Added version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
+//    id("com.android.application")
+//    id("org.jetbrains.kotlin.android")
+//    id("com.google.dagger.hilt.android")
+//    id("com.google.devtools.ksp")
 }
 
 android {
@@ -43,22 +47,42 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.junit)
+    implementation(libs.junit)
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3.compose)
 
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
+    implementation(libs.dagger.hilt.android)
+    ksp(libs.dagger.hilt.compiler)
+    implementation(libs.hilt.navigation.fragment)
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
+//    implementation("androidx.core:core-ktx:1.13.1")
+//    implementation("androidx.appcompat:appcompat:1.7.0")
+//    implementation("com.google.android.material:material:1.12.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+//    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+//    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    implementation("com.google.dagger:hilt-android:2.51.1")
+//    implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
+
+//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
+
+//    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.dagger.hilt.android") version "2.49" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt.android) apply false
+
+
+//    id("com.android.application") version "8.5.2" apply false
+//    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+//    id("com.google.dagger.hilt.android") version "2.49" apply false
+//    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
 }
